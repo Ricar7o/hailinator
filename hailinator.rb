421785610f5051926b873891144fe1f14361abb1
MakerSquare Student <student@macintosh.att.net>
2013-06-19 09:53:37 -0500

    Added twitter config method

---
+++ hailinator.rb
@@ -0,0 +1,8 @@
+require "twitter"
+
+Twitter.configure do |config|
+  config.consumer_key = o9QwqK8swZ2v5nuWikNfYA
+  config.consumer_secret = 8uws74UiOZrrqab6KeqV4aYbOoknL4TKVoAx3uApk
+  # config.oauth_token = YOUR_OAUTH_TOKEN
+  # config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+end
