8c78ecb13707031028b6dc908c71a813864b584e
MakerSquare Student <student@macintosh.att.net>
2013-06-19 10:01:48 -0500

    step 4 pass

--- hailinator.rb
+++ hailinator.rb
@@ -1,8 +1,11 @@
 require "twitter"
 
 Twitter.configure do |config|
-  config.consumer_key = o9QwqK8swZ2v5nuWikNfYA
-  config.consumer_secret = 8uws74UiOZrrqab6KeqV4aYbOoknL4TKVoAx3uApk
-  # config.oauth_token = YOUR_OAUTH_TOKEN
-  # config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  config.consumer_key = "o9QwqK8swZ2v5nuWikNfYA"
+  config.consumer_secret = "8uws74UiOZrrqab6KeqV4aYbOoknL4TKVoAx3uApk"
+  config.oauth_token = "1455072764-ejsvYUdDJKZc3S7xlgG29Mr44Tbs3HTzMHZE8jg"
+  config.oauth_token_secret = "VIaB7USezEdEBbLIhzIDYJSvSCw2c4ziyEYS9r2Wxf4"
 end
+
+# hack to connect to twitter
+Twitter.home_timeline
